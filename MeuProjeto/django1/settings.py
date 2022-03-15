cd32713a3d1ea22d862cb147e68f31cc0ab2f481
Lucas Lima <57276011+LucasUfac@users.noreply.github.com>
2022-03-15 16:45:24 -0500

    enter

--- MeuProjeto/django1/settings.py
+++ MeuProjeto/django1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^7qc96%rt0b#2mz=z1&z0n@03ucrmcxymqa=of5h4i2=jze_l3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
