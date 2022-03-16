db1eccbff04d702509210fd52ebe244dab277f2b
Lucas Lima <57276011+LucasUfac@users.noreply.github.com>
2022-03-16 17:08:04 -0500

    fix

--- MeuProjeto/core/models.py
+++ MeuProjeto/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Produto(models.Model):
-    nome = models.CharField('Nome', max_length=100),
+    nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em estoque')
 
