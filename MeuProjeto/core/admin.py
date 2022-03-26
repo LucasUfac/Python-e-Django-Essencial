3c228e7d2db89d7703ca2fa076bb705d6c6b52ce
Lucas Lima <15lucas.lima15@gmail.com>
2022-03-26 17:23:07 -0500

    increment

--- MeuProjeto/core/admin.py
+++ MeuProjeto/core/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Produto, Cliente
 
-admin.site.register(Produto)
-admin.site.register(Cliente)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("nome","preco", "estoque")
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nome", "sobrenome", "email")
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Cliente, ClienteAdmin)
--- MeuProjeto/core/models.py
+++ MeuProjeto/core/models.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.db import models
 
 class Produto(models.Model):
@@ -5,8 +6,14 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em estoque')
 
+    def __str__(self) -> str:
+        return self.nome
+
 
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
     sobrenome = models.CharField('Sobrenome', max_length=100)
     email = models.EmailField('Email', max_length=100)
+
+    def __str__(self) -> str:
+        return f'{self.nome} {self.sobrenome}'
